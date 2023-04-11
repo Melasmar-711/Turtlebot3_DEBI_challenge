165142c3cb3b76a7b1b1e2ac61ccb37d644bc0fb
Mahmoud Elasmar <84791015+Melasmar-711@users.noreply.github.com>
2023-04-11 23:14:35 +0200

    Delete build directory

--- build/turtlebot3_manipulation/turtlebot3_manipulation_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asmar/Tutrtlebot3_DEBI_Robotics_challenge/src/turtlebot3_manipulation/turtlebot3_manipulation_bringup
-# Build directory: /home/asmar/Tutrtlebot3_DEBI_Robotics_challenge/build/turtlebot3_manipulation/turtlebot3_manipulation_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
