beeceab5673cd4d724b065f8e60786f1eb7a4242
Melasmar-711 <mahmoudelasmar2000@outlook.com>
2023-04-17 17:14:09 +0200

    updated control_arm.py

--- src/debi/scripts/aa.py
+++ src/debi/scripts/aa.py
@@ -1,35 +1,72 @@
+#!/usr/bin/env python
+
 import sys
-import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import tf
+import numpy as np
 
-
-
-moveit_commander.roscpp_initialize(sys.argv)
+def move_to_pose(x,y,z):
+    # Initialize moveit_commander and rospy node
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_to_pose', anonymous=True)
     
-rospy.init_node('move_robotic_arm', anonymous=True)
-                
-                
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
+    listener = tf.TransformListener()
+    target_frame = 'base_footprint'
 
-# We can get the name of the reference frame for this robot:
-planning_frame = group.get_planning_frame()
-print ("============ Reference frame: %s" % planning_frame)
+    listener.waitForTransform(target_frame,'odom', rospy.Time(), rospy.Duration(4.0))
+    (trans, rot) = listener.lookupTransform(target_frame,'odom',  rospy.Time(0))
+    homogeneous_matrix = tf.transformations.concatenate_matrices(tf.transformations.translation_matrix(trans),tf.transformations.quaternion_matrix(rot))
+    
+    point_world_homogeneous = np.array([x, y, z, 1.0])
+    point_planning_homogeneous = np.dot(homogeneous_matrix, point_world_homogeneous)
+    x,y,z = (point_planning_homogeneous[0], point_planning_homogeneous[1], point_planning_homogeneous[2])
+    print(x,y,z)
 
-# We can also print the name of the end-effector link for this group:
-eef_link = group.get_end_effector_link()
-print( "============ End effector: %s" % eef_link)
+    # Create a RobotCommander object to control the robot's arms and planning scene
+    robot = moveit_commander.RobotCommander()
 
-# We can get a list of all the groups in the robot:
-group_names = robot.get_group_names()
-print ("============ Robot Groups:", robot.get_group_names())
+    # Create a PlanningSceneInterface object to access the planning scene
+    scene = moveit_commander.PlanningSceneInterface()
 
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print ("============ Printing robot state")
-print (robot.get_current_state())
-print ("")
-                
+    # Create a MoveGroupCommander object for the group of joints you want to control
+    group_name = "arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+    
+    move_group.set_pose_reference_frame('odom')
+
+    # Set the maximum time MoveIt will plan for a motion plan
+    move_group.set_planning_time(15)
+
+    # Set the target pose for the end effector
+    target_pose = geometry_msgs.msg.Pose()
+    target_pose.position.x =  x
+    target_pose.position.y =  0
+    target_pose.position.z = z
+    #target_pose.orientation.x = 0.0
+    #target_pose.orientation.y = 0.0
+    #target_pose.orientation.z = 0.0
+    #target_pose.orientation.w = 1.0
+
+    # Set the target pose for the arm to move to
+    move_group.set_pose_target(target_pose)
+
+    # Plan and execute the motion
+    plan = move_group.go(wait=True)
+    if plan:
+        rospy.loginfo("Motion plan successful!")
+    else:
+        rospy.logerr("Motion plan failed!")
+    
+    # Clean up
+    moveit_commander.roscpp_shutdown()
+
+
+if __name__ == '__main__':
+    try:
+        move_to_pose(1.2,1.4,0.11)
+    except rospy.ROSInterruptException:
+        pass
+
--- src/debi/scripts/control_arm.py
+++ src/debi/scripts/control_arm.py
@@ -4,32 +4,52 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-import sys
+import tf
+import numpy as np
+
+
+
 
 def move_to_absolute_pose(x, y, z):
     # Initialize moveit_commander and rospy
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('move_robotic_arm', anonymous=True)
+    #listener = tf.TransformListener()
+    #target_frame = 'base_footprint'
 
+    #listener.waitForTransform(target_frame, 'odom', rospy.Time(), rospy.Duration(4.0))
+    #(trans, rot) = listener.lookupTransform(target_frame, 'odom', rospy.Time(0))
+    #homogeneous_matrix = tf.transformations.concatenate_matrices(tf.transformations.translation_matrix(trans),tf.transformations.quaternion_matrix(rot))
+    
+    #point_world_homogeneous = np.array([x, y, z, 1.0])
+    #point_planning_homogeneous = np.dot(homogeneous_matrix, point_world_homogeneous)
+    #x,y,z = (point_planning_homogeneous[0], point_planning_homogeneous[1], point_planning_homogeneous[2])
+    #print(x,y,z)
+    
+    
+    
+    
     # Set up the planning group
     group_name = "arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
-
+    #move_group.set_pose_reference_frame("base_link")
+    eef_link = move_group.get_end_effector_link()
+    planning_frame = move_group.get_planning_frame()
+    print(planning_frame)
     # Set the target pose for the end effector
+
     pose_target = geometry_msgs.msg.Pose()
-    pose_target.header.frame_id = "odom"
-    pose_target.orientation.w = 1.0
+    #pose_target.orientation.w = 1.0
     pose_target.position.x = x
     pose_target.position.y = y
     pose_target.position.z = z
+
 
     # Set the target pose
     move_group.set_pose_target(pose_target)
 
     # Plan and execute the motion
-    plan = move_group.go(wait=True)
-    move_group.stop()
-    move_group.clear_pose_targets()
+    move_group.go(wait=True)
 
     # Shutdown moveit_commander and rospy
     moveit_commander.roscpp_shutdown()
@@ -37,5 +57,7 @@ def move_to_absolute_pose(x, y, z):
 
 if __name__=='__main__':
     # Move the end effector to an absolute position
-    move_to_absolute_pose(1.17, 1.4, 0.27)
+
+    
+    move_to_absolute_pose(0.24, 0, 0.11)
 
